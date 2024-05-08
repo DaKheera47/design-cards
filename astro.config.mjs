ed564692eff4fe3eae0d611e519d7234be33556e
DaKheera47 <shaheergoodboy@gmail.com>
2024-05-08 10:32:49 +0100

    vercel images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  adapter: vercel({
+    imageService: true,
+  }),
 });
